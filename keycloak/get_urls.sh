fde8c0ae99e106afa83406fbd53bba3a61d24395
Jay Flesher <jay@deb-linux-laptop1.flesher.local>
2023-10-15 14:02:20 -0400

    update

---
+++ keycloak/get_urls.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "" &&
+echo "Keycloak:                 https://keycloak.192.168.0.240.nip.io" &&
+echo "Keycloak Admin Console:   https://keycloak.192.168.0.240.nip.io/admin" &&
+echo "Keycloak Account Console: https://keycloak.192.168.0.240.nip.io/realms/myrealm/account" &&
+echo ""
