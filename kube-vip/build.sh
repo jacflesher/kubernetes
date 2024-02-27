d5d5418ad32d06187d9cdd1f61d4c87503926758
Jay Flesher <jac.flesher@gmail.com>
2024-02-27 16:37:29 -0500

    update

--- kube-vip/build.sh
+++ kube-vip/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+kubectl apply -f rbac.yaml
+kubectl apply -f daemonset.yaml
 kubectl apply -f kube-vip-cloud-controller.yaml
 kubectl create configmap -n kube-system kubevip --from-literal range-global=192.168.0.240-192.168.0.243
