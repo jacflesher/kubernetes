924c14931d6a98046b5c4b3a03aebd9405a87c83
jacflesher <61653964+jacflesher@users.noreply.github.com>
2024-02-28 08:03:04 -0500

    Update create-secret.sh

--- cert-manager/create-secret.sh
+++ cert-manager/create-secret.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-[ $(command -v argocd-vault-plugin) ] || echo "Missing 'argocd-vault-plugin'... Exiting."
-[ $(command -v argocd-vault-plugin) ] || exit 1
+[ $(command -v argocd-vault-plugin) ] || { echo "Missing 'argocd-vault-plugin'... Exiting."; exit 1; }
 export AVP_TYPE=gcpsecretmanager
 gcloud auth application-default login --no-launch-browser
 cat secret.yaml | argocd-vault-plugin generate -
