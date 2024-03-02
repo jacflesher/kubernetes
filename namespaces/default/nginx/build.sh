ea278c0d52970066eba834904250e48d2056a73e
Jay Flesher <jay@linux-01.flesher.local>
2024-03-02 09:51:36 -0500

    update

---
+++ namespaces/default/nginx/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ -z "$2" ] && { echo "Must pass CERT file path and KEY file path as ARG 1 and ARG 2, respectively... Aborting."; exit 1; }
+[ -f "$1" ] || { echo "Cert file $1 not found... Aborting."; exit 1; }
+[ -f "$2" ] || { echo "Key file $2 not found... Aborting."; exit 1; }
+NS="default"
+kubectl create configmap www-flesher-app-configmap --from-file index.html --namespace $NS
+kubectl apply -f deployment.yaml --namespace $NS
+kubectl apply -f service.yaml --namespace $NS
+kubectl create secret tls www-flesher-app-tls --cert "$1" --key "$2" --namespace $NS
+kubectl apply -f ingress-tls.yaml --namespace $NS
