be716554dad4c9921892b74c57bfb1abcebd172b
Jay Flesher <jac.flesher@gmail.com>
2024-09-21 10:57:22 -0400

    update

--- kube-vip/build.sh
+++ kube-vip/build.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+
+VIP=192.168.0.244
+INTERFACE=eth0
+KVVERSION=$(curl -sL https://api.github.com/repos/kube-vip/kube-vip/releases | jq -r ".[0].name")
+
+ctr image pull ghcr.io/kube-vip/kube-vip:$KVVERSION; ctr run --rm --net-host ghcr.io/kube-vip/kube-vip:$KVVERSION vip /kube-vip manifest pod \
+--interface $INTERFACE \
+--address $VIP \
+--controlplane \
+--services \
+--arp \
+--leaderElection | \
+tee kube-vip.yaml
+
+curl -s -o rbac.yaml https://kube-vip.io/manifests/rbac.yaml
 kubectl apply -f rbac.yaml
-kubectl apply -f daemonset.yaml
-kubectl apply -f kube-vip-cloud-controller.yaml
-kubectl create configmap -n kube-system kubevip --from-literal range-global=192.168.0.240-192.168.0.243
+
+ctr image pull ghcr.io/kube-vip/kube-vip:$KVVERSION; ctr run --rm --net-host ghcr.io/kube-vip/kube-vip:$KVVERSION vip /kube-vip manifest daemonset \
+    --interface $INTERFACE \
+    --address $VIP \
+    --inCluster \
+    --taint \
+    --controlplane \
+    --services \
+    --arp \
+    --leaderElection
