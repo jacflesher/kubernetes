b85b7e28ab1eb61e336d8e55a5f16b2472bc4822
Jay <jay@flesher.app>
2024-11-28 12:25:02 -0500

    update

--- hostmappingdemo/cert_install.sh
+++ hostmappingdemo/cert_install.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
 set +x
 
-[ $(command -v yq) ] || { echo "Must have 'yq' binary installed... Aborting."; exit 1; }
-[ -z "$2" ] && { echo "Must pass CERT file path and KEY file path as ARG 1 and ARG 2, respectively... Aborting."; exit 1; }
-[ -f "$1" ] || { echo "Cert file $1 not found... Aborting."; exit 1; }
-[ -f "$2" ] || { echo "Key file $2 not found... Aborting."; exit 1; }
-
-#echo "Apply cert to which namespace?"
-#kubectl get namespaces | sed 1d | awk -F' ' '{print $1}'
-#echo
-#read -r NS
-
+if [ -z "$VAULT_ADDR" ]; then 
+    echo "Not logged into vault..."
+    read -r -p "Enter vault address (https://host:8200): " VAULT_ADDR
+    export VAULT_ADDR
+fi
+if [ -z "$GH_ENTERPRISE_TOKEN" ]; then
+    read -r -p "Enter vault github token: " GH_ENTERPRISE_TOKEN
+    export GH_ENTERPRISE_TOKEN
+fi
+export AVP_TYPE="vault"
+export AVP_AUTH_TYPE="github"
+export AVP_GITHUB_TOKEN="$GH_ENTERPRISE_TOKEN"
 NS="ns-jay"
 
-yq <<EOF
+# argocd-vault-plugin generate - --verbose-sensitive-output
+cat <<EOF | argocd-vault-plugin generate - | yq
 ---
 apiVersion: v1
 kind: Secret
+type: kubernetes.io/tls
 metadata:
   name: ${NS}-tlsstore
   namespace: ${NS}
 data:
-  tls.crt: $(base64 < "$1" | tr -d '\n')
-  tls.key: $(base64 < "$2" | tr -d '\n')
-type: kubernetes.io/tls
+  tls.crt: <path:secrets/data/certbot/flesher.app#cert|base64encode>
+  tls.key: <path:secrets/data/certbot/flesher.app#key|base64encode>
 EOF
-echo
-echo "Confirm? (Y/n)"
-read -r CONFIRM
-[[ "${CONFIRM}" =~ ^(YES|Yes|yes|Y|y)$ ]] || exit 1
 
-yq <<EOF | kubectl apply -f - --server-side --force-conflicts
+echo
+echo
+echo "Apply changes?"
+read -r APPLY
+
+[[ "$APPLY" =~ ^(Yes|YES|yes|y|Y)$ ]] || exit 
+
+cat <<EOF | argocd-vault-plugin generate - | kubectl apply -f -
 ---
 apiVersion: v1
 kind: Secret
+type: kubernetes.io/tls
 metadata:
   name: ${NS}-tlsstore
   namespace: ${NS}
 data:
-  tls.crt: $(base64 < "$1" | tr -d '\n')
-  tls.key: $(base64 < "$2" | tr -d '\n')
-type: kubernetes.io/tls
+  tls.crt: <path:secrets/data/certbot/flesher.app#cert|base64encode>
+  tls.key: <path:secrets/data/certbot/flesher.app#key|base64encode>
 EOF
